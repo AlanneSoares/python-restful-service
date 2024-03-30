92dccb64f533651c84ab714b2d68d64b56549f22
Alanne Soares <alannesilva@yahoo.com.br>
2024-03-30 00:53:09 -0300

    Implementation api python

--- main.py
+++ main.py
@@ -1,16 +1,88 @@
-# This is a sample Python script.
+from flask import Flask, jsonify, request, json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+HOST = "localhost"
+PORT = "4999"
+
+with open('datas/restaurants.json') as file:
+    restaurants = json.load(file)
+
+## Insert
+@app.route("/api/restaurants", methods=["POST"])
+def insert():
+    restaurant = request.json
+    restaurants.append(restaurant)
+    return jsonify({"restaurant": restaurant}), 201
+
+## Find all
+@app.route("/api/restaurants", methods=["GET"])
+def find_all():
+    return jsonify({"restaurants": restaurants}), 200
+
+## Find by id
+@app.route("/api/restaurants/<int:restaurant_id>", methods=["GET"])
+def find_by_id(restaurant_id: int):
+    for restaurant in restaurants:
+        if restaurant.get("id") == restaurant_id:
+            return jsonify({"restaurant": restaurant}), 200
+    return jsonify({"error": "Internal Server Error"}), 500
+
+## Find by postal code (with params)
+@app.route("/api/restaurants/address", methods=["GET"])
+def find_by_postal_code():
+    postalcode = request.args.get("postalcode")
+    for restaurant in restaurants:
+        if restaurant.get("address").get("postalCode") == postalcode:
+            return jsonify({"restaurant": restaurant}), 200
+    return jsonify({"error": "Internal Server Error"}), 500
+
+## Find by postal code (in the request body)
+@app.route("/api/restaurants/address", methods=["POST"])
+def find_by_postal_code_address():
+    postalcode = request.json.get("postalCode")
+    for restaurant in restaurants:
+        if restaurant.get("address").get("postalCode") == postalcode:
+            return jsonify({"restaurant": restaurant}), 200
+    return jsonify({"error": "Internal Server error"}), 500
+
+## Find by street address
+# def findByStreetAddress():
+
+## Find by address region
+# def findByAddressRegion():
+
+## Find by address country
+# def findByAddressCountry():
+
+## Update address by id
+@app.route("/api/restaurants/<int:restaurant_id>/address", methods=["PUT"])
+def update_address_by_id(restaurant_id: int):
+    changed_restaurant = request.json
+    for index, restaurant in enumerate(restaurants):
+        if restaurant.get("id") == restaurant_id:
+            restaurants[index].update(changed_restaurant)
+            return jsonify({"restaurant": restaurants[index]}), 200
+    return jsonify({"error": "Internal Server Error"}), 500
 
 
-#def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+## Update postal code
+# def updatePostalCode():
+
+## Update street address
+# def updateStreetAddress():
+
+## Update address locality
+# def updateAddressLocality():
+
+## Update address region
+# def updateAddressRegion():
+
+## Update address country
+# def updateAddressCountry():
+
+## Delete by id
+# def deleteById(id):
 
 
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-    #print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(HOST, PORT, debug=True)
